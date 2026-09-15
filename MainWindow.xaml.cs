421dce8ebfd6e3ae035c797a207d359399e37d75
Luisa Ramirez <ramirezluisa150@gmail.com>
2026-09-14 19:46:54 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+using System.Windows;
+namespace CS077.RepeatedSignals;
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    // Tutorial 3: add the event handler and loop. No solution is supplied here.
+}
